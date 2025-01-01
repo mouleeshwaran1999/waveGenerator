caa8a7769da78e4c890946c0cbec1f5eb33cd091
Your Name <your-email@example.com>
2025-01-02 02:58:10 +0530

    fixed cors issue

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 3000;
 const appRoutes = require('./routes/routes')
 
 // Middleware to parse JSON bodies
+app.use(cors());
 app.use(express.json());
 
 app.use('/',appRoutes)
